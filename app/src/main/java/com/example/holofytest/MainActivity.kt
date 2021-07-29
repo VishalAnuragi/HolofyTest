39ceb1910f5bd8293b41afe565ee6c84c01682cc
Vishal <vishalanuragi123@gmail.com>
2021-07-30 01:24:38 +0530

    videoview recycler

--- app/src/main/java/com/example/holofytest/MainActivity.kt
+++ app/src/main/java/com/example/holofytest/MainActivity.kt
@@ -2,10 +2,92 @@ package com.example.holofytest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.DefaultItemAnimator
+import androidx.recyclerview.widget.LinearLayoutManager
+
+
+import android.widget.AbsListView
+
+
+
 
 class MainActivity : AppCompatActivity() {
+
+    var videoRecycler : RecyclerView ?= null
+   // var videoAdapter : VideoAdapter ?= null
+    var mData : List<VideoModal> ?= null
+    var lastItem : Int = 0
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        initRecycler()
+
+
     }
+
+    private fun initRecycler() {
+
+
+        val videomodal: Array<VideoModal> = arrayOf<VideoModal>(
+            VideoModal("Bird", "Child"  ,"android.resource://" + packageName + "/" + com.example.holofytest.R.raw.eggs),
+            VideoModal("Ball", "Ronald"  ,"android.resource://" + packageName + "/" + com.example.holofytest.R.raw.ball),
+            VideoModal("Bird", "Child"  ,"android.resource://" + packageName + "/" + com.example.holofytest.R.raw.eggs),
+            VideoModal("Ball", "Ronald"  ,"android.resource://" + packageName + "/" + com.example.holofytest.R.raw.ball),
+            VideoModal("Bird", "Child"  ,"android.resource://" + packageName + "/" + com.example.holofytest.R.raw.eggs),
+            VideoModal("Ball", "Ronald"  ,"android.resource://" + packageName + "/" + com.example.holofytest.R.raw.ball),
+            )
+
+        videoRecycler = findViewById(R.id.videoRecycler)
+        videoRecycler!!.layoutManager = LinearLayoutManager( applicationContext , LinearLayoutManager.VERTICAL , false)
+        videoRecycler!!.setHasFixedSize(true)
+
+        val videoadapter = VideoAdapter(applicationContext , videomodal)
+        videoRecycler!!.adapter = videoadapter
+        videoRecycler!!.itemAnimator = DefaultItemAnimator()
+
+        addScrollListener()
+
+    }
+
+    private fun addScrollListener() {
+
+        videoRecycler!!.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+            override fun onScrollStateChanged(
+                recyclerView: RecyclerView,
+                newState: Int
+            ) {
+                super.onScrollStateChanged(recyclerView, newState)
+                if (newState == AbsListView.OnScrollListener.SCROLL_STATE_IDLE) {
+                    //get the recyclerview position which is completely visible and first
+                    val positionView =
+                        (videoRecycler!!.getLayoutManager() as LinearLayoutManager).findFirstCompletelyVisibleItemPosition()
+                    Log.i("VISISBLE", positionView.toString() + "")
+                  /*  if (positionView >= 0) {
+                        if (oldFocusedLayout != null) {
+                            //Stop the previous video playback after new scroll
+                            oldFocusedLayout.findViewById(R.id.vv_dashboard).stopPlayback()
+                        }
+                        currentFocusedLayout =
+                            (rv_featuredList.getLayoutManager() as LinearLayoutManager).findViewByPosition(
+                                positionView
+                            )
+                        val vv_dashboard = currentFocusedLayout.findViewById(R.id.vv_dashboard)
+                        //to play video of selected recylerview, videosData is an array-list which is send to recyclerview adapter to fill the view. Here we getting that specific video which is displayed through recyclerview.
+                        playVideo(videosData.get(positionView))
+                        oldFocusedLayout = currentFocusedLayout
+                    }*/
+                }
+
+
+            }
+        })
+    }
+
+
 }
--- app/src/main/java/com/example/holofytest/VideoAdapter.kt
+++ app/src/main/java/com/example/holofytest/VideoAdapter.kt
@@ -2,6 +2,7 @@ package com.example.holofytest
 
 import android.content.Context
 import android.net.Uri
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -12,6 +13,8 @@ import androidx.recyclerview.widget.RecyclerView
 
 var packageName : String ?= "com.example.holofytest"
 var vidLink : String ?= null
+var visibleItem : Int ?= 0
+var p : Int  ?= 0
 
 class VideoAdapter (
     private val context: Context,
@@ -21,32 +24,43 @@ class VideoAdapter (
     class ImageViewHolder(view: View) : RecyclerView.ViewHolder(view) {
 
         val itemCard = itemView.findViewById<CardView>(R.id.itemCard)
-        val itemVideo = itemView.findViewById<VideoView>(R.id.itemVideo)
+        var itemVideo  = itemView.findViewById<VideoView>(R.id.itemVideo)
         val itemTitle = itemView.findViewById<TextView>(R.id.itemTitle)
         val itemSubTitle = itemView.findViewById<TextView>(R.id.itemSubTitle)
 
 
+
         fun bindView(vid: VideoModal) {
-            itemVideo.setVideoURI(Uri.parse(vidLink))
+
+            vidLink = vid.videoURL
             itemTitle.text = vid.title
             itemSubTitle.text = vid.subtitle
+
+            itemVideo.setVideoPath(vidLink);
+
+                itemVideo.start();
+
+
+            var fi : MainActivity = MainActivity()
+
+            visibleItem = fi.lastItem
+            Log.i("TAG123" , visibleItem.toString())
         }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ImageViewHolder =
         ImageViewHolder(LayoutInflater.from(context).inflate(R.layout.item_videos, parent, false))
 
+
     override fun getItemCount(): Int = arr.size
 
     override fun onBindViewHolder(holder: ImageViewHolder, position: Int) {
+
         holder.bindView(arr[position])
+        p = position
 
-        when(position) {
+        Log.i("TAG123" , p.toString())
 
-            0 -> vidLink = "android.resource://" + packageName + "/" + R.raw.eggs
-            1 -> vidLink = "android.resource://" + packageName + "/" + R.raw.eggs
-            2 -> vidLink = "android.resource://" + packageName + "/" + R.raw.eggs
-        }
 
         holder.itemCard.setOnClickListener {
 
